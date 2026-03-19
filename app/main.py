f2cda489242d294bd8015167273ba659a0cbaf36
Ren Shevchuk <shevchuk_re@ukr.net>
2026-03-19 13:01:46 +0000

    added rate limit for analysis

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from app.routers.healthcheck import router as health_router
 from app.routers.document import router as doc_router
 from app.routers.users import router as auth_router
 from fastapi.middleware.cors import CORSMiddleware
+from app.core.rate_limit import limiter
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
 
 app = FastAPI(
     title=settings.title,
@@ -11,12 +15,16 @@ app = FastAPI(
     openapi_version="3.0.3",
 )
 
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 allowed_org = settings.cors_origins
 
 app.include_router(health_router)
 app.include_router(doc_router)
 app.include_router(auth_router)
 
+app.add_middleware(SlowAPIMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_org or ["http://localhost:3000"],
